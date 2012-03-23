12dfd1b1a27446d5d8c4f0a184296dc54116ffc4
natew <natewienert@gmail.com>
2012-03-23 04:55:37 -0700

    proper deleting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -86,7 +86,7 @@ class PostsController < ApplicationController
 		@post.destroy
 
 		respond_to do |format|
-			format.html { redirect_to posts_url }
+			format.html { redirect_to '/admin' }
 			format.xml { head :ok }
 		end
 	end
