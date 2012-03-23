9a17ea31fcea6d2c991029fb3790899e090763f6
natew <natewienert@gmail.com>
2012-03-23 00:26:33 -0700

    working editing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Obtvse::Application.routes.draw do
   resources :posts
   match '/admin', to: 'posts#admin'
   match '/new', to: 'posts#new'
-  match '/edit/:id', to: 'posts#edit', as: 'post'
-  match '/:slug', to: 'posts#show', as: 'post'
+  match '/edit/:id', to: 'posts#edit'
+  put '/:slug', to: 'posts#update', as: 'post'
+  get '/:slug', to: 'posts#show'
   root to: 'posts#index'
 end
