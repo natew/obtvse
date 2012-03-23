0cf4b1c0b88475d9c69d9f2c6c550ddace452107
Jon Morton <jon.morton@gmail.com>
2012-03-23 09:45:09 -0400

    Add some tests for the posts controller

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def test_anyone_can_list_the_posts
+    get :index
+    assert_response :ok
+  end
+  
+  def test_spies_cannot_spy_by_listing_drafts
+    get :index
+    assert_response :ok
+  end
+  
+  def test_spies_cannot_use_the_admin_list
+    get :admin
+    assert_response :unauthorized
+  end
+  
+  def test_anyone_can_read_a_post
+    get :show, slug: 'codename-obtvse'
+    assert_response :ok
+  end
+  
+  def test_spies_cannot_spy_by_reading_drafts
+    get :show, slug: 'a-modest-proposal'
+    assert_response :not_found
+  end
+  
+  def test_haters_cannot_hate
+    delete :destroy, slug: 'codename-obtvse'
+    assert_response :unauthorized
+  end
+  
+  def test_haters_cannot_even_try_to_hate
+    delete :destroy, slug: 'thats-not-even-a-real-post'
+    assert_response :unauthorized
+  end
+  
+  def test_vandals_cannot_vandalize_by_updating
+    put :update, slug: 'codename-obtvse', content: 'all your base are belong to us'
+    assert_response :unauthorized
+  end
+  
+  def test_vandals_cannot_vandalize_by_creating
+    post :create, slug: 'im-with-stupid', content: 'again, all your base are belong to us'
+    assert_response :unauthorized
+  end
+  
 end
