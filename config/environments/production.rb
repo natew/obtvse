a4b94a4007afad2dcc09d16269f425dcf2b52b54
natew <natewienert@gmail.com>
2012-03-27 05:53:45 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,3 @@ Obtvse::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # Reset Rails's object cache
-    # Only works with DalliStore
-    Rails.cache.reset if forked
-
-    # Reset Rails's session store
-    # If you know a cleaner way to find the session store instance, please let me know
-    ObjectSpace.each_object(ActionDispatch::Session::DalliStore) { |obj| obj.reset }
-  end
-end
