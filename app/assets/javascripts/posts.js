ac73244cc86d826e7eb0470968cd71d1c0939208
natew <natewienert@gmail.com>
2012-03-27 04:44:08 -0700

    Feature: Automatically push down window when we are adding new lines on a post Feature: Preview working, opens in new window Bugfix: Expanding textareas work Some styling cleanup Auto-save initial JS commented out in posts.js Fix memcached bugs in development, remove Passenger stuff

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,12 +1,48 @@
 $(function() {
 	// Auto-expanding height for editor textareas
-	var areas = document.querySelectorAll('.expandingArea');
-	var l = areas.length;
+	var title = document.getElementById('text-title'),
+	    content = document.getElementById('text-content');
 
-	while (l--) {
-	  makeExpandingArea(areas[l]);
+	// If we're on the edit page
+	if (title) {
+		// Auto expanding textareas.  See _functions.js
+		makeExpandingArea(title);
+		makeExpandingArea(content);
+
+		// Scroll window if we edit long posts
+		$('#post_content').bind('keyup', function() {
+			var $this = $(this),
+			    bottom = $this.offset().top + $this.height();
+
+			if (bottom > $(window).scrollTop() &&
+				$this.prop("selectionStart") > ($this.val().length - $this.val().split('\n').slice(-1)[0].length)) {
+				$(window).scrollTop(bottom);
+			}
+		});
+
+		// Set minimum height of content textarea
+		$('#post_content').css('min-height', $(window).height() - $('#post_title').height() - 130);
+
+		// Autosave
+		// setInterval(function(){
+		//   var form = $('.edit_post'),
+		//       action = form.attr('action');
+
+		//   $.post(action, form.serialize(), function(data){
+		//   	$('body').prepend('<span>Saved!</span>');
+		//   });
+		// },10000);
+
+		// Preview pops open new window
+		var form = document.getElementsByTagName('form')[0];
+		document.getElementById('preview-button').onclick = function() {
+	    form.target = '_blank';
+		}
+		document.getElementById('save-button').onclick = function() {
+	    form.target = '_self';
+		}
+
+		// Fade out save post notice
+		$('.notice').delay(2000).fadeOut(500);
 	}
-
-	// Set minimum height of content textarea
-	$('#post_content').css('min-height', $(window).height() - $('#post_title').height() - 130);
 });
