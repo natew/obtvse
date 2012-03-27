00a095dcf081e3700ecdb48ffe439b281ffbf1f0
natew <natewienert@gmail.com>
2012-03-27 15:27:46 -0700

    put and post preview

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Obtvse::Application.routes.draw do
   match '/admin', :to => 'posts#admin'
   match '/new', :to => 'posts#new'
   match '/edit/:id', :to => 'posts#edit'
+  post '/preview', :to => 'posts#preview'
   put '/preview', :to => 'posts#preview'
   get '/:slug', :to => 'posts#show', :as => 'post'
   delete '/:slug', :to => 'posts#destroy', :as  => 'post'
