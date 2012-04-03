ffe5689d7d8ba579ea8db6ab38636b7f2e93ba20
Pablo Merino <pablo.perso1995@gmail.com>
2012-04-03 20:50:07 +0200

    fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,9 @@ Obtvse::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+  config.assets.precompile += %w( mobile.css.scss mobile.js )
+
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
