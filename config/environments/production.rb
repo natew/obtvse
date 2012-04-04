d7a6b6214bfcf51f6acaa7e0a411fe424dfc30e8
natew <natewienert@gmail.com>
2012-04-04 00:25:16 -0700

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Obtvse::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.reset_password_mailer = UserMailer
 
   # Enable threaded mode
   # config.threadsafe!
