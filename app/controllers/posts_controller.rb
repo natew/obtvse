d945343f6a14285d4738aabf52c12980a84703fb
natew <natewienert@gmail.com>
2012-04-04 21:54:14 -0700

    fix editing so save works on new and on old

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,25 +56,27 @@ class PostsController < ApplicationController
 			if @post.save
 				format.html { redirect_to "/edit/#{@post.id}", :notice => "Post created successfully" }
 				format.xml { render :xml => @post, :status => :created, location: @post }
-				format.text { render :text => @post.id }
+				format.text { render :text => "#{@post.id},#{@post.slug}" }
 			else
 				format.html { render :action => 'new' }
 				format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
-				format.text { render :status => 500 }
+				format.text { head :bad_request }
 			end
 		end
 	end
 
 	def update
-		@post = Post.find_by_slug(params[:slug])
+		@post = Post.find(params[:id])
 
 		respond_to do |format|
 			if @post.update_attributes(params[:post])
 				format.html { redirect_to "/edit/#{@post.id}", :notice => "Post updated successfully" }
 				format.xml { head :ok }
+				format.text { head :ok }
 			else
 				format.html { render :action => 'edit' }
 				format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
+				format.text { head :bad_request }
 			end
 		end
 	end
