25ff55cb97498bb0add1437594b20391df485468
natew <natewienert@gmail.com>
2012-04-04 21:54:14 -0700

    fix editing so save works on new and on old

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Obtvse::Application.routes.draw do
   resources :posts
   match '/get/:id', :to => 'posts#get'
   match '/new', :to => 'posts#new'
+  post '/edit/:id', :to => 'posts#update', :as  => 'post'
   match '/edit/:id', :to => 'posts#edit'
   get '/:slug', :to => 'posts#show', :as => 'post'
   delete '/:slug', :to => 'posts#destroy', :as  => 'post'
-  put '/:slug', :to => 'posts#update', :as  => 'post'
   root :to => 'posts#index'
 end
