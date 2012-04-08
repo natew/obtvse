fcfda6b590cb696758ca50d8dc12293b369130c1
natew <natewienert@gmail.com>
2012-04-08 02:09:01 -0700

    getting ready for image uploads

---
+++ db/migrate/20120408085017_create_images.rb
@@ -0,0 +1,7 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+  	create_table :images do |t|
+  		t.string :image_file_name, :image_file_size, :image_content_type
+  		t.timestamp :image_updated_at
+  end
+end
