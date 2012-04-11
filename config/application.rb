e016e664e52711b349fc2467f136d19fbb586bec
natew <natewienert@gmail.com>
2012-04-10 19:42:41 -0700

    precompile adminjs

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Obtvse
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
+    # Precompile admin assets
+    config.assets.precompile += ['admin.js']
+
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
