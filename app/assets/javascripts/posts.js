1c2b90816fb801716e1abd067bd4c76c58d31051
natew <natewienert@gmail.com>
2012-04-15 00:31:53 -0700

    blah

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,5 +2,5 @@ $(function() {
   $('.post').fitVids();
 
   $('header').addClass('transition');
-  setTimeout(function(){$('header').addClass('hued')},1000);
+  setTimeout(function(){$('header').addClass('hued')},400);
 });
