2e1d3f5acf3f374a8fbf5f95d067d676cb9c4011
natew <natewienert@gmail.com>
2012-04-15 17:39:42 -0700

    stylist fixes

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -3,4 +3,9 @@ $(function() {
 
   $('header').addClass('transition');
   setTimeout(function(){$('header').addClass('hued')},400);
+
+  $('#back-to-top').click(function(e) {
+    e.preventDefault();
+    $('body').animate({scrollTop:0});
+  });
 });
