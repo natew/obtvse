ef9cf68fa8adc62b79f9e03e1726f8bf657faaa0
natew <natewienert@gmail.com>
2012-04-18 08:35:59 -0700

    quickpush

--- app/assets/javascripts/admin/history.js
+++ app/assets/javascripts/admin/history.js
@@ -9,6 +9,20 @@
   History.Adapter.bind(window,'statechange',function windowStateChange(){
     var State = History.getState();
     fn.log(State.data, State.title, State.url);
+
+    switch(State.title) {
+      case 'admin':
+
+        break;
+
+      case 'edit':
+
+        break;
+
+      case 'new':
+
+        break;
+    }
   });
 
 })(window, fn);
