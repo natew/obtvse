c2d0c9f994d2c80f49cc5be8173c72a871bc6378
Kay Rhodes <krhodes@rbmtechnologies.com>
2012-04-23 15:48:33 -0400

    replaced tabs in posts_controller w/spaces for consistency
    posts_controller.b seemed to be the only file in the app
    that was using tabs so I replaced them with the rails standard
    of two spaces for consistency with the rest of the app.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,20 @@
 require 'rdiscount'
 
 class PostsController < ApplicationController
-	before_filter :authenticate, :except => [:index, :show]
-	layout :choose_layout
+  before_filter :authenticate, :except => [:index, :show]
+  layout :choose_layout
 
-	def index
-		@posts = Post.page(params[:page]).per(10).where(draft:false)
+  def index
+    @posts = Post.page(params[:page]).per(10).where(draft:false)
 
-		respond_to do |format|
-			format.html
-			format.xml { render :xml => @posts }
-			format.rss { render :layout => false }
-		end
-	end
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @posts }
+      format.rss { render :layout => false }
+    end
+  end
 
-	def preview
+  def preview
     @post = Post.new(params[:post])
     @preview = true
     respond_to do |format|
@@ -22,96 +22,96 @@ class PostsController < ApplicationController
     end
   end
 
-	def admin
-		@no_header = true
-		@post = Post.new
-		@published = Post.where(draft:false).page(params[:post_page]).per(20)
-		@drafts = Post.where(draft:true).page(params[:draft_page]).per(20)
+  def admin
+    @no_header = true
+    @post = Post.new
+    @published = Post.where(draft:false).page(params[:post_page]).per(20)
+    @drafts = Post.where(draft:true).page(params[:draft_page]).per(20)
 
-		respond_to do |format|
-			format.html
-		end
-	end
+    respond_to do |format|
+      format.html
+    end
+  end
 
-	def show
-		@single_post = true
-		@post = admin? ? Post.find_by_slug(params[:slug]) : Post.find_by_slug_and_draft(params[:slug],false)
+  def show
+    @single_post = true
+    @post = admin? ? Post.find_by_slug(params[:slug]) : Post.find_by_slug_and_draft(params[:slug],false)
 
-		respond_to do |format|
-			if @post.present?
-				format.html
-				format.xml { render :xml => @post }
-			else
-				format.any { head status: :not_found  }
-			end
-		end
-	end
+    respond_to do |format|
+      if @post.present?
+        format.html
+        format.xml { render :xml => @post }
+      else
+        format.any { head status: :not_found  }
+      end
+    end
+  end
 
-	def new
-		@no_header = true
-		@posts = Post.page(params[:page]).per(20)
-		@post = Post.new
+  def new
+    @no_header = true
+    @posts = Post.page(params[:page]).per(20)
+    @post = Post.new
 
-		respond_to do |format|
-			format.html
-			format.xml { render xml: @post }
-		end
-	end
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @post }
+    end
+  end
 
-	def edit
-		@no_header = true
-		@post = Post.find(params[:id])
-	end
+  def edit
+    @no_header = true
+    @post = Post.find(params[:id])
+  end
 
-	def create
-		@post = Post.new(params[:post])
+  def create
+    @post = Post.new(params[:post])
 
-		respond_to do |format|
-			if @post.save
-				format.html { redirect_to "/edit/#{@post.id}", :notice => "Post created successfully" }
-				format.xml { render :xml => @post, :status => :created, location: @post }
-			else
-				format.html { render :action => 'new' }
-				format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
-			end
-		end
-	end
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to "/edit/#{@post.id}", :notice => "Post created successfully" }
+        format.xml { render :xml => @post, :status => :created, location: @post }
+      else
+        format.html { render :action => 'new' }
+        format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
+      end
+    end
+  end
 
-	def update
-		@post = Post.find_by_slug(params[:slug])
+  def update
+    @post = Post.find_by_slug(params[:slug])
 
-		respond_to do |format|
-			if @post.update_attributes(params[:post])
-				format.html { redirect_to "/edit/#{@post.id}", :notice => "Post updated successfully" }
-				format.xml { head :ok }
-			else
-				format.html { render :action => 'edit' }
-				format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
-			end
-		end
-	end
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html { redirect_to "/edit/#{@post.id}", :notice => "Post updated successfully" }
+        format.xml { head :ok }
+      else
+        format.html { render :action => 'edit' }
+        format.xml { render :xml => @post.errors, :status => :unprocessable_entity}
+      end
+    end
+  end
 
-	def destroy
-		@post = Post.find_by_slug(params[:slug])
-		@post.destroy
+  def destroy
+    @post = Post.find_by_slug(params[:slug])
+    @post.destroy
 
-		respond_to do |format|
-			format.html { redirect_to '/admin' }
-			format.xml { head :ok }
-		end
-	end
+    respond_to do |format|
+      format.html { redirect_to '/admin' }
+      format.xml { head :ok }
+    end
+  end
 
-	private
+  private
 
-	def admin?
-		session[:admin] == true
-	end
+  def admin?
+    session[:admin] == true
+  end
 
-	def choose_layout
-		if ['admin', 'new', 'edit', 'create'].include? action_name
-			'admin'
-		else
-			'application'
-		end
-	end
+  def choose_layout
+    if ['admin', 'new', 'edit', 'create'].include? action_name
+      'admin'
+    else
+      'application'
+    end
+  end
 end
