ace0470d5c15f906a3e17f4b95c252371c034dec
Craig Wickesser <craig@mindscratch.org>
2012-04-25 20:34:21 -0400

    removed dependency on javascript library for embedding gists, using simple ajax and custom css to style gists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,24 @@ module ApplicationHelper
 
   def markdown(text)
     text = youtube_embed(text)
+<<<<<<< HEAD
     r = RDiscount.new(text)
     r.smart = true
     r
+=======
+    text = gist_embed(text)
+    RedcarpetCompat.new(text, :fenced_code, :gh_blockcode)
+  end
+
+  # TODO refactor these filters so they don't each iterate over all the lines
+  def gist_embed(str)
+    output = str.lines.map do |line|
+      match = nil
+      match = line.match(/\{\{gist\s+(.*)\}\}/)
+      match ? "<div id=\"#{match[1]}\" class=\"gist\">Loading gist...</div>" : line
+    end
+    output.join
+>>>>>>> c647a5a... removed dependency on js library to embed gists, now using simple ajax call and css to get and style gists
   end
 
   def youtube_embed(str)
