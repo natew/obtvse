26379ab59892bcf374dd6d70f1c98ff7c2c25225
Craig Wickesser <craig@mindscratch.org>
2012-04-26 21:55:29 -0400

    refactored gist embedding

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,29 +1,5 @@
 $(function() {
 	$('.post').fitVids();
 
-  var printGist = function(gist) {
-      console.log(gist.repo, ' (' + gist.description + ') :');
-      console.log(gist.div);
-  };
-
-  var displayGist = function(selector, gistResponse) {
-    $(selector).html(gistResponse.div);
-  };
-
-  // TODO: bad, don't add functions to jQuery
-  $.getGist = function(id, success) {
-    $.ajax({
-          url: 'https://gist.github.com/' + id + '.json',
-          dataType: 'jsonp',
-          success: function(gist) {
-            success("#" + id, gist);
-          }
-      });
-  };
-
-  // fetch the gists
-  $('.gist').each(function() {
-    $.getGist(this.id, displayGist);
-  });
-
+  Obtvse.renderGists('.gist-files');
 });
