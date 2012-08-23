ab75fbb4fed1e42935994f178582a3ba7a6b6837
Joel Moss <joel@developwithstyle.com>
2012-08-23 21:39:19 +0100

    Config.yml is no longer required, instead setting a couple of defaults in the absence of a config file.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,24 @@
-CONFIG = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'config.yml'))).result)
+if (config = Rails.root.join('config', 'config.yml')).exist?
+  CONFIG = YAML::load(ERB.new(IO.read(config)).result)
+else
+  CONFIG = {
+    'title' =>    'Obtvse Demo',
+    'tagline' =>  'Clever tagline!',
+    'login' =>    ENV["obtvse_login"] ? ENV["obtvse_login"] : "username",
+    'password' => ENV["obtvse_password"] ? ENV["obtvse_password"] : "password"
+  }
+end
+
+
+
+# ---
+# title:               DWS
+# tagline:             Developing with Style
+# login:               <%= ENV["obtvse_login"] ? ENV["obtvse_login"] : "username" %>
+# password:            <%= ENV["obtvse_password"] ? ENV["obtvse_password"] : "password" %>
+# name:                Joel Moss
+# twitter:             joelmoss
+# github:              joelmoss
+# email:               joel@developwithstyle.com
+# google_analytics_id: YOUR_GA_ID
+# disqus_shortname:    <%= ENV.fetch("disqus_shortname") { "joelmoss" } %>
