2bc5c69044135de599cd2188f51f38b20eccbedf
natew <natewienert@gmail.com>
2013-01-17 22:35:09 -0800

    Fixups

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Obtvse::Application.config.secret_token = '6fb26b28c7a1c88fbbe61168259f716902623dfdc10bff236e1926c69c1e81c1ceb2b5011e700afc721a494a4a82d8ad1d71a19a00c8483bacf99dad5bf22d8b'
+Obtvse::Application.config.secret_token = 'jbdb8i023bdu9h98gbfofdipfhwe[0hpflneuig4t9o23eiwbjhfcodusi fewhiowyr8dhi 38129greo2diuy3it4728t3go1`e8ydwusfg7o84243rebdfoi8frvg7238owygdbvyik'
